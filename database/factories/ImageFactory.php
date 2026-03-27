5949a72fb9aabf16a68d15556670af0dfc18ef81
Aleix <aleix.murt22@lacetania.cat>
2026-03-27 15:52:59 +0100

    Canvi en la lògica de ImgeFactory ;

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class ImageFactory extends Factory
 {
@@ -12,15 +14,16 @@ class ImageFactory extends Factory
      */
     public function definition(): array
     {
+        // Descarreguem una imatge aleatòria de picsum
+        $filename = Str::random(10) . '.jpg';
+        $path = storage_path('app/public/images/' . $filename);
+
+        $imageContent = file_get_contents('https://picsum.photos/640/480?random=' . rand(1, 1000));
+        file_put_contents($path, $imageContent);
+
         return [
             'user_id'     => User::factory(),
-            'image_path'  => 'images/' . fake()->image(
-                                storage_path('app/public/images'),
-                                640,
-                                480,
-                                null,
-                                false
-                             ),
+            'image_path'  => 'images/' . $filename,
             'description' => fake()->sentence(),
         ];
     }
