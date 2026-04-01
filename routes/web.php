b3494f77bf5654c830983452091100c6af7e3ab0
usuario <aleix.murt22@lacetania.cat>
2026-04-01 19:18:27 +0200

    canvis de rutes

--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,21 @@ use App\Http\Controllers\UserController;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect()->route('home');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+// ImageController Routes
+Route::get('/images/{image}', [ImageController::class, 'show'])->name('images.show');
+
+Route::get('/images/{image}/data', [ImageController::class, 'data'])->name('images.data');
+
+Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    // ImageController Routes
-    Route::get('/images/{image}', [ImageController::class, 'show'])->name('posts.show');
-    Route::get('/images/{image}/data', [ImageController::class, 'data'])->name('images.data');
-
+    
     // CommentCobtroller Routes
     Route::post('/images/{image}/comments', [CommentController::class, 'store'])->name('comments.store');
     Route::get('/comments/{comment}/edit', [CommentController::class, 'edit'])->name('comments.edit');
@@ -32,16 +35,13 @@ Route::middleware('auth')->group(function () {
     
     // LikeController Routes
     Route::post('/images/{image}/like', [LikeController::class, 'toggle'])->name('likes.toggle');
-
+    
     // Posts CRUD Routes
     Route::get('/posts/create', [ImageController::class, 'create'])->name('posts.create');
     Route::post('/posts', [ImageController::class, 'store'])->name('posts.store');
     Route::get('/posts/{image}/edit', [ImageController::class, 'edit'])->name('posts.edit');
     Route::patch('/posts/{image}', [ImageController::class, 'update'])->name('posts.update');
-    Route::delete('/posts/{image}', [ImageController::class, 'destroy'])->name('posts.destroy');
-
-    // ProfileController Routes
-    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::delete('/posts/{image}', [ImageController::class, 'destroy'])->name('posts.destroy');    
 });
-
+    
 require __DIR__.'/auth.php';
